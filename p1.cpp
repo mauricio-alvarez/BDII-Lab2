14000c3dbf4390a39cc2ce65c5723217271e94cc
Mauricio Alvarez <83974344+mauricio-alvarez@users.noreply.github.com>
2023-03-31 16:06:24 -0500

    Parte b - lista

--- p1.cpp
+++ p1.cpp
@@ -5,13 +5,19 @@
 
 using namespace std;
 
-
 struct Alumno{
 	char codigo[5];
 	char nombre[11];
 	char apellidos[20];
 	char carrera[15];
 };
+
+void print(Alumno alumno){
+	cout <<	"Codigo: " << alumno.codigo <<
+	"\nNombre: " << alumno.nombre <<
+	"\nApellidos: " << alumno.apellidos <<
+	"\nCarrera: " << alumno.carrera << endl;	
+}
 
 class FixedRecord{
 private:
@@ -27,14 +33,20 @@ public:
 		inFile.seekg(0,inFile.beg);
 	};
 	
-	vector<Alumno> load_a(){
+	vector<Alumno> load(){
 		vector<Alumno> alumnos;
 		if (inFile.is_open()){
-			Alumno temp;
-			while (!inFile.eof()) {
-				cout << temp.nombre << endl;
-				alumnos.push_back(temp);
-			}
+            // allocate mem
+            int cantity = this->length / 51;
+            for (int i =0;i<cantity;i++){
+                Alumno a;
+                inFile.seekg(51*i+(2*i));
+                inFile.get(a.codigo,5,'\0');
+                inFile.get(a.nombre, 11,'\0');
+                inFile.get(a.apellidos,20,'\0');
+                inFile.get(a.carrera,15,'\0');    
+                alumnos.push_back(a);
+            }
 			inFile.close();			
 		}else {	
 			cout <<"File Cant be Read" <<endl;
@@ -50,51 +62,17 @@ public:
 	}
 };
 
-void print(Alumno alumno){
-	cout <<	"Codigo: " << alumno.codigo <<
-	"\nNombre: " << alumno.nombre <<
-	"\nApellidos: " << alumno.apellidos <<
-	"\nCarrera: " << alumno.carrera << endl;
-	
-}
-int main(){
-	/*FixedRecord file("datos1.txt");
-	vector<Alumno> alumnos = file.load();
-	cout << "Data Loaded!!" << endl;
-	for (auto alumno: alumnos){
-		cout << "Codigo: " << alumno.codigo <<
-		", Nombre: " << alumno.nombre <<
-		", Apellidos: " << alumno.apellidos <<
-		", Carrera: " << alumno.carrera << endl;
+void ejercicio_b(vector<Alumno> vec){
+    for (auto alumno: vec){
+		print(alumno);
 	};
-	*/
-	ifstream file("datos1.txt");
-	file.seekg(0,file.end);
-	int length = file.tellg();
-	file.seekg(0,file.beg);
-	// allocate mem
-	char* buffer = new char[length];
+}
+
+int main(){
+    FixedRecord file("datos1.txt");
+	vector<Alumno> vec = file.load();
+	//ejercicio_b(vec);
+    
 	
-	//file.read(buffer, length);
-	int cantity = length / 51;
-	int n=0,let=0;	
-	file.seekg(n);
-	//for (int i=0; i<cantity;i++){
-		Alumno a;
-		file.get(a.codigo,5);
-		file.get(a.nombre, 11);
-		file.get(a.apellidos,20);
-		file.get(a.carrera,15);
-		print(a);
-		char del[5];
-		file.seekg(150);
-		file.get(del,5,'\0');	
-		file.get(del,5);
-		cout<<del<<endl;
-		cout<<file.tellg()<<endl;
-	
-	file.close();
-//	cout.write(buffer, length);
-	delete [] buffer;
 	return 0;
 }
