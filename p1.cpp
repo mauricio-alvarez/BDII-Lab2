983c33971a5b6dc22c74acb6205c5af7c465dd21
mauricio alvarez <mauricio.alvarez@utec.edu.pe>
2023-04-01 12:01:21 -0500

    Write func

--- p1.cpp
+++ p1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <ostream>
 #include <vector>
 
 using namespace std;
@@ -12,6 +13,17 @@ struct Alumno{
 	char carrera[15];
 };
 
+ostream& operator<<(ostream& stream, Alumno& a){
+	stream << "\n";
+	stream << a.codigo;
+	stream << a.nombre;
+	stream << a.apellidos;
+	stream << a.carrera;
+	stream << '\0';
+	stream << flush;
+	return stream;
+}
+
 void print(Alumno alumno){
 	cout <<	"Codigo: " << alumno.codigo <<
 	"\nNombre: " << alumno.nombre <<
@@ -19,15 +31,27 @@ void print(Alumno alumno){
 	"\nCarrera: " << alumno.carrera << endl;	
 }
 
+void readFromConsole(char buffer[], int size){
+	string temp;
+	cin >> temp;
+	for (int i=0; i<temp.size(); i++) {
+		buffer[i] = (i<temp.size())? temp[i]:' ';
+	}
+	buffer[size-1] = '\0';
+	cin.clear();
+}
+
 class FixedRecord{
 private:
 	ifstream inFile;
+	ofstream outFile;
 	int length;
 
 public:
 	FixedRecord() = default;
 	FixedRecord(string filename) {
-		inFile.open(filename);	
+		inFile.open(filename);
+		outFile.open(filename,fstream::app);
 		inFile.seekg(0,inFile.end);
 		this->length = inFile.tellg();
 		inFile.seekg(0,inFile.beg);
@@ -54,7 +78,21 @@ public:
 		return alumnos;
 	}
 	void add(Alumno record){
-		return;
+		if (outFile.is_open()){
+			// ask from console
+			cout <<"Codigo: "; readFromConsole(record.codigo,5);
+			cout <<"Nombres: "; readFromConsole(record.nombre,11);
+			cout <<"Apellidos: "; readFromConsole(record.apellidos,20);
+			cout <<"Carrera: "; readFromConsole(record.carrera,15);
+			//outFile << record;
+			outFile << "\n";
+			outFile.write(record.codigo, 5);
+			outFile.write(record.nombre,11);
+			outFile.write(record.apellidos, 20);
+			outFile.write(record.carrera, 15);
+			outFile<<"\0"; outFile<<flush;
+			outFile.close();
+		}
 	}
 	Alumno readRecord(int pos){
 		Alumno temp;
@@ -69,10 +107,11 @@ void ejercicio_b(vector<Alumno> vec){
 }
 
 int main(){
-    FixedRecord file("datos1.txt");
+	FixedRecord file("datos1.txt");
+	Alumno alumno;
+	file.add(alumno);
 	vector<Alumno> vec = file.load();
 	ejercicio_b(vec);
-    
-	
+
 	return 0;
 }
