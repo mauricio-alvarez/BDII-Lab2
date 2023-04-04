db5d6b1428b53a1fa9a529a19df3b8c33db435f2
LuisFern873 <luis.mendez.l@utec.edu.pe>
2023-04-04 16:25:34 -0500

    p3 arreglado

--- p3.cpp
+++ p3.cpp
@@ -1,264 +1,135 @@
-#include <fstream>
 #include <iostream>
-#include <vector>
-#include <string>
 #include <fstream>
-#include <cstring>
+#include <vector>
 #include <sstream>
-#include <sys/stat.h>
-#include <filesystem>
 
 using namespace std;
 
-struct Alumno{
-	char codigo[5];
-	char nombre[11];
-	char apellidos[20];
-	char carrera[15];
+struct Alumno 
+{     
+    string nombre;   
+    string apellidos;   
+    string carrera;   
+    float mensualidad; 
+};
 
-     int ciclo;
-     float mensualidad;
+// For writing a new record to the file
+ostream& operator<<(ostream& os, const Alumno& student)
+{
+    os << student.nombre << '|';
+    os << student.apellidos << '|';
+    os << student.carrera << '|';
+    os << student.mensualidad << '\n';
+    os << flush;
+    return os;
+}
 
-     int nextDel = -1;
+// For reading a new record from the file
+istream& operator>>(istream& is, Alumno& student)
+{
+    getline(is, student.nombre, '|');
+    getline(is, student.apellidos, '|');
+    getline(is, student.carrera, '|');
+    is >> student.mensualidad;
+    return is;
+}
+
+template <template <typename...> class Container, typename T>
+void print(const Container<T>& container, ostream& os = cout)
+{
+    for (const T& elem : container) {
+        os << elem << " ";
+    }
+    os << "\n";
+}
+
+class VariableRecord
+{
+    public:
+    VariableRecord(string fileName);
+    Alumno readRecord(int pos);
+    vector<Alumno> load();
+    void add(Alumno record);
+
+    private:
+    string fileName;
 };
 
 
+VariableRecord::VariableRecord(string fileName) : fileName(fileName)
+{
 
-class FixedRecord{
-private:
-     string fileName;
-public:
-     FixedRecord(string fileName) : fileName(fileName){
-          ofstream file(fileName, ios::trunc);
-          file.close();
-     };
-     void add(Alumno alumno){
-          
-          
-          if(this->headerData()==-2){
-               fstream file(fileName, ios::app | ios::binary);
-               file.write((alumno.codigo), 5);
-               file.write((alumno.nombre), 11);
-               file.write((alumno.apellidos), 20);
-               file.write((alumno.carrera), 15);
+}
 
-               file.write((char *)(&alumno.ciclo), sizeof(int));
-               file.write((char *)(&alumno.mensualidad), sizeof(float));
+// O(n), n: number of records in the file
+Alumno VariableRecord::readRecord(int pos)
+{
+    ifstream file;
+    file.open(fileName);
 
-               file.write((char *)(&alumno.nextDel), sizeof(int));
-               file << '\n';
-               file.close();
-          }else{
-               //Recuperamos el ultimo eliminado del registro desactivado
-               fstream file(fileName, ios::binary | ios::in | ios::out);
-               int aux_1;file.seekp(64*headerData()+5+59);
-               file.read((char *)&aux_1, sizeof(int));
+    Alumno student;
 
-               //Volcamos el nuevo registro sobre el antiguo
-               file.seekp(64*headerData()+5, ios::beg);
-               file.write((alumno.codigo), 5);
-               file.write((alumno.nombre), 11);
-               file.write((alumno.apellidos), 20);
-               file.write((alumno.carrera), 15);
+    int currentPos = 0;
+    int currentRecord = 1;
 
-               file.write((char *)(&alumno.ciclo), sizeof(int));
-               file.write((char *)(&alumno.mensualidad), sizeof(float));
+    while (currentRecord < pos && file >> student) {
+        currentPos = file.tellg();
+        currentRecord++;
+    }
 
-               file.write((char *)(&alumno.nextDel), sizeof(int));
+    if (currentRecord == pos && file >> student) {
+        // Read the desired record from the file
+    } else {
+        throw invalid_argument("Index out of range");
+    }
 
-               //Cambiamos el header con el aux_1
-               file.seekp(0);
-               file.write((char *)&aux_1, sizeof(int));
+    file.close();
 
-               cout<<"En el header: "<<aux_1<<endl;
-               
-               file.close();
-          }
-          
-
-          
-     }
-     vector<Alumno> loadAll(){
-          vector<Alumno> students;
-          Alumno student;
-          ifstream file(fileName,std::ifstream::ate | ios::binary);
-          
-          int registerSize = file.tellg()/64;
-          file.seekg(0, ios::beg);
-
-          for (int i = 0; i < registerSize; i++){
-               file.seekg(sizeof(student)*i+5, ios::beg);
-
-               file.read(student.codigo, sizeof(student.codigo));
-               file.read(student.nombre, sizeof(student.nombre));
-               file.read(student.apellidos, sizeof(student.apellidos));
-               file.read(student.carrera, sizeof(student.carrera));
-
-               file.read((char *)&student.ciclo, sizeof(int));
-               file.read((char *)&student.mensualidad, sizeof(float));
-               file.read((char *)&student.nextDel, sizeof(int));
-
-               students.push_back(student);
-          }          
-          file.close();
-          cout<<"Se encontro "<<students.size()<<", registros."<<endl;
-          return students;
-     }
-     Alumno readRecord(int pos){
-          Alumno student;
-          ifstream file(fileName,std::ifstream::ate | ios::binary);
-          int registerSize = file.tellg()/sizeof(student);file.seekg(0, std::ios::beg);
-          if(registerSize >= pos){          
-               file.seekg(sizeof(student)*pos, ios::beg);
-
-               file.read(student.codigo, sizeof(student.codigo));
-               file.read(student.nombre, sizeof(student.nombre));
-               file.read(student.apellidos, sizeof(student.apellidos));
-               file.read(student.carrera, sizeof(student.carrera));
-
-               file.read((char *)&student.ciclo, sizeof(int));
-               file.read((char *)&student.mensualidad, sizeof(float));
-          }else{
-               cout<<"Index out of range"<<endl;
-          }
+    return student;
+}
 
 
-          return student;
-     }
+// O(n), n: number of records in the file
+vector<Alumno> VariableRecord::load()
+{
+    ifstream file;
+    file.open(fileName);
 
-     int headerData(){
-          ifstream mak(fileName,std::ifstream::ate | ios::binary);
-          mak.seekg(0, ios::beg);
-          int aux;mak.read((char *)&aux, sizeof(int));
-          mak.close();
-          return aux;
-     }
+    vector<Alumno> students;
+    Alumno student;
 
-     bool _delete(int pos){
-          //Recuperamos lo que habia en el header
-          int aux = headerData();
+    while (file >> student) {
+        students.emplace_back(student);
+    }
 
-          //Escribimos la posicion del valor eliminado. 
-          fstream file(fileName, ios::binary | ios::in | ios::out);
-          file.seekp(0);
-          file.write((char *)&pos, sizeof(int));
-          
-          //Escribimos en la posicion eliminada el contenido del header
-          file.seekp(sizeof(Alumno)*pos+5+59);
-          file.write((char *)&aux, sizeof(int));
+    file.close();
 
-          file.close();
-          cout<<headerData()<<endl;
-          return true;
-     }
+    return students;
+}
 
-     void setHeader(){
-          ifstream in(fileName,std::ifstream::ate | ios::binary);
-          int registerSize = in.tellg();in.seekg(0, std::ios::beg);
-          in.close();
-          ofstream out(fileName, ios::app | ios::binary);int fisrDelete = -2;
-          out.write((char*)(&fisrDelete), sizeof(int));
-          out << '\n';
-          out.close();
-     }
-     int getTotalSize(){
-          ifstream in(fileName,std::ifstream::ate | ios::binary);
-          int registerSize = in.tellg();in.seekg(0, std::ios::beg);
-          in.close();
-          return registerSize;
-     }
+// O(1)
+void VariableRecord::add(Alumno record)
+{
+    ofstream file;
+    file.open(fileName, ios::app);
+    file << record;
+    file.close();
+}
 
-     vector<Alumno> load(){
-          vector<Alumno> students;
-          Alumno student;
-          ifstream file(fileName,std::ifstream::ate | ios::binary);
-          
-          int registerSize = file.tellg()/64;
-          file.seekg(0, ios::beg);
+int main() 
+{
+    string fileName = "datos2.txt";
+    VariableRecord VR(fileName);
 
-          for (int i = 0; i < registerSize; i++){
-               file.seekg(sizeof(student)*i+5, ios::beg);
+    vector<Alumno> students = VR.load();
+    print(students);
 
-               file.read(student.codigo, sizeof(student.codigo));
-               file.read(student.nombre, sizeof(student.nombre));
-               file.read(student.apellidos, sizeof(student.apellidos));
-               file.read(student.carrera, sizeof(student.carrera));
+    Alumno newStudent = {"Luis", "Mendez", "Computer Science", 2000};
 
-               file.read((char *)&student.ciclo, sizeof(int));
-               file.read((char *)&student.mensualidad, sizeof(float));
-               file.read((char *)&student.nextDel, sizeof(int));
+    VR.add(newStudent);
 
-               if(student.nextDel == -1){
-                    students.push_back(student);
-               }
-          }          
-          file.close();
-          cout<<"Se encontro "<<students.size()<<", registros."<<endl;
-          return students;
-     }
+    Alumno bufferStudent = VR.readRecord(6);
+    cout << bufferStudent;
 
-
-
-};
-
-int main() {
-     string _file = "datos1.txt";
-     FixedRecord fr(_file);
-
-     fr.setHeader();
-     //cout<<"Total tienes: "<<fr.getTotalSize()<<endl;
-
-     Alumno a1 = {"1231","AARON     ", "SANT               ", "CS            ",5,233.4};
-     Alumno a2 = {"1232","RODRIGO   ", "AVENDAN            ", "MECATRONIC    ",2,2332.4};
-     Alumno a3 = {"1233","TAVO      ", "LAMI LAMI LAMI     ", "PSICOLOGIA    ",4,2622.2};
-     Alumno a4 = {"1234","ALVARO    ", "ALLAUCA VLADIMIR   ", "CS            ",1,22.2};
-     Alumno a5 = {"1235","MARIO SUSA", "ALLAUCA ZOTO       ", "PSICOLOGIA    ",2,22.2};
-     Alumno a6 = {"1236","ALVARITO  ", "ALLAUCA EDASON     ", "FISICA        ",3,22.2};
-     Alumno a7 = {"1237","DANIELS   ", "ALLAUCA MARSOON    ", "INGENIERIA CIV",4,22.2};
-     fr.add(a1);
-     fr.add(a2);
-     fr.add(a3);
-     fr.add(a4);
-     fr.add(a5);
-     fr.add(a6);
-     
-
-     auto registers = fr.load();
-     
-     for (auto it = registers.begin(); it != registers.end(); it++){
-          cout<<(*it).codigo<<"  "<<(*it).nombre<<"  "<<(*it).apellidos<<"  "<<(*it).carrera<<"  "<<(*it).ciclo<<"  "<<(*it).mensualidad<<" "<<(*it).nextDel<<endl;
-     }
-     fr._delete(2);
-
-     fr.add(a7);
-
-     registers = fr.loadAll();
-     
-     for (auto it = registers.begin(); it != registers.end(); it++){
-          cout<<(*it).codigo<<"  "<<(*it).nombre<<"  "<<(*it).apellidos<<"  "<<(*it).carrera<<"  "<<(*it).ciclo<<"  "<<(*it).mensualidad<<" "<<(*it).nextDel<<endl;
-     }
-
-     /*
-     fr._delete(2);
-     fr._delete(4);
-
-     registers = fr.loadAll();
-
-     for (auto it = registers.begin(); it != registers.end(); it++){
-          cout<<(*it).codigo<<"  "<<(*it).nombre<<"  "<<(*it).apellidos<<"  "<<(*it).carrera<<"  "<<(*it).ciclo<<"  "<<(*it).mensualidad<<" "<<(*it).nextDel<<endl;
-     }
-
-     registers = fr.load();
-
-     for (auto it = registers.begin(); it != registers.end(); it++){
-          cout<<(*it).codigo<<"  "<<(*it).nombre<<"  "<<(*it).apellidos<<"  "<<(*it).carrera<<"  "<<(*it).ciclo<<"  "<<(*it).mensualidad<<" "<<(*it).nextDel<<endl;
-     }*/
-     //fr.readRecord(2);
-
-
-     
-
-
-
-     return 0;
+    return 0;
 }
