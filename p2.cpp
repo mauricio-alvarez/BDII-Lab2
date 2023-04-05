9ff9bd5bfc76fe77644d45406a758d2cb28e52b2
AronSantamaria <83294705+AronSantamaria@users.noreply.github.com>
2023-04-05 18:29:47 -0500

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -33,19 +33,45 @@ public:
           file.close();
      };
      void add(Alumno alumno){
-          ofstream file(fileName, ios::app | ios::binary);
-          file.write((alumno.codigo), 5);
-          file.write((alumno.nombre), 11);
-          file.write((alumno.apellidos), 20);
-          file.write((alumno.carrera), 15);
+          
+          
+          if(this->headerData()==-2){
+               fstream file(fileName, ios::app | ios::binary);
+               file.write((alumno.codigo), 5);
+               file.write((alumno.nombre), 11);
+               file.write((alumno.apellidos), 20);
+               file.write((alumno.carrera), 15);
+               file.write((char *)(&alumno.ciclo), sizeof(int));
+               file.write((char *)(&alumno.mensualidad), sizeof(float));
+               file.write((char *)(&alumno.nextDel), sizeof(int));
+               file << '\n';
 
-          file.write((char *)(&alumno.ciclo), sizeof(int));
-          file.write((char *)(&alumno.mensualidad), sizeof(float));
+               file.close();
+          }else{
+               //Recuperamos el ultimo eliminado del registro desactivado
+               fstream file(fileName, ios::binary | ios::in | ios::out);
+               int aux_1;file.seekp(64*headerData()+5+59);
+               file.read((char *)&aux_1, sizeof(int));
 
-          file.write((char *)(&alumno.nextDel), sizeof(int));
-          file << '\n';
+               //Volcamos el nuevo registro sobre el antiguo
+               file.seekp(64*headerData()+5, ios::beg);
+               file.write((alumno.codigo), 5);
+               file.write((alumno.nombre), 11);
+               file.write((alumno.apellidos), 20);
+               file.write((alumno.carrera), 15);
+               file.write((char *)(&alumno.ciclo), sizeof(int));
+               file.write((char *)(&alumno.mensualidad), sizeof(float));
+               file.write((char *)(&alumno.nextDel), sizeof(int));
 
-          file.close();
+               //Cambiamos el header con el aux_1
+               file.seekp(0);
+               file.write((char *)&aux_1, sizeof(int));
+               
+               file.close();
+          }
+          
+
+          
      }
      vector<Alumno> loadAll(){
           vector<Alumno> students;
@@ -62,23 +88,19 @@ public:
                file.read(student.nombre, sizeof(student.nombre));
                file.read(student.apellidos, sizeof(student.apellidos));
                file.read(student.carrera, sizeof(student.carrera));
-
                file.read((char *)&student.ciclo, sizeof(int));
                file.read((char *)&student.mensualidad, sizeof(float));
                file.read((char *)&student.nextDel, sizeof(int));
 
-
                students.push_back(student);
           }          
           file.close();
-          cout<<"Se encontro "<<students.size()<<", registros."<<endl;
           return students;
      }
      Alumno readRecord(int pos){
           Alumno student;
           ifstream file(fileName,std::ifstream::ate | ios::binary);
           int registerSize = file.tellg()/sizeof(student);file.seekg(0, std::ios::beg);
-          cout<<"Hay:  "<<registerSize<<endl;
           if(registerSize >= pos){          
                file.seekg(sizeof(student)*pos, ios::beg);
 
@@ -86,17 +108,8 @@ public:
                file.read(student.nombre, sizeof(student.nombre));
                file.read(student.apellidos, sizeof(student.apellidos));
                file.read(student.carrera, sizeof(student.carrera));
-
                file.read((char *)&student.ciclo, sizeof(int));
                file.read((char *)&student.mensualidad, sizeof(float));
-
-               cout<<student.codigo<<endl;
-               cout<<student.nombre<<endl;
-               cout<<(student.apellidos)<<endl;
-               cout<<student.carrera<<endl;
-
-               cout<<student.ciclo<<endl;
-               cout<<student.mensualidad<<endl;
 
           }else{
                cout<<"Index out of range"<<endl;
@@ -115,11 +128,12 @@ public:
      }
 
      bool _delete(int pos){
-          //Recuperamos lo que habia en el header
+          //Recuperamos el header
           int aux = headerData();
 
-          //Escribimos la posicion del valor eliminado. 
+          //Escribimos la posicion del valor a eliminar. 
           fstream file(fileName, ios::binary | ios::in | ios::out);
+          //cout<<"Size register:  "<<file.tellg()/sizeof(Alumno);file.seekg(0, std::ios::beg);
           file.seekp(0);
           file.write((char *)&pos, sizeof(int));
           
@@ -128,7 +142,6 @@ public:
           file.write((char *)&aux, sizeof(int));
 
           file.close();
-          cout<<headerData()<<endl;
           return true;
      }
 
@@ -171,10 +184,8 @@ public:
                if(student.nextDel == -1){
                     students.push_back(student);
                }
-               
           }          
           file.close();
-          cout<<"Se encontro "<<students.size()<<", registros."<<endl;
           return students;
      }
 
@@ -182,27 +193,13 @@ public:
 
 };
 
-int main() {
-     string _file = "datos1.txt";
-     FixedRecord fr(_file);
-
-     
-     Alumno a1 = {"1234","AARON     ", "SANT               ", "CS            ",5,233.4};
-     Alumno a2 = {"1235","RODRIGO   ", "AVENDAN            ", "MECATRONIC    ",2,2332.4};
-     Alumno a3 = {"1236","TAVO      ", "LAMI LAMI LAMI     ", "PSICOLOGIA    ",4,2622.2};
-     Alumno a4 = {"1237","ALVARO    ", "ALLAUCA VLADIMIR   ", "CS            ",1,22.2};
-     fr.add(a1);
-     fr.add(a2);
-     fr.add(a3);
-     fr.add(a4);
-     
-     //fr.load();
-     fr.readRecord(2);
-
-
-     //cout<<sizeof(a1)<<endl;
-
-
-
-     return 0;
+std::string aleatorio(int longitud_maxima) {
+    std::string caracteres_permitidos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int longitud = longitud_maxima;
+    std::string texto = "";
+    for (int i = 0; i < longitud; i++) {
+        int indice = rand() % caracteres_permitidos.length();
+        texto += caracteres_permitidos[indice];
+    }
+    return texto;
 }
